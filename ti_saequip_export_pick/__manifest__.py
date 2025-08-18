76e20525c23bcffec71e32455495a4a503293a91
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2025-08-18 15:16:27 +0545

    fix dependent model

--- ti_saequip_export_pick/__manifest__.py
+++ ti_saequip_export_pick/__manifest__.py
@@ -3,7 +3,7 @@
     "author": "Target Integration",
     "category": "Warehouse Management",
     "version": "18.0.1.0",
-    "depends": ["stock"],
+    "depends": ["stock", "stock_delivery"],
     "website": "https://targetintegration.com/",
     "data": ["views/export_action_button.xml", "views/export_enable_checkbox.xml"],
     "license": "LGPL-3",
