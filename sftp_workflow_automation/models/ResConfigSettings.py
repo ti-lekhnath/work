b0f312947a1eafdfab31183c1f23ccb288e86228
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2025-08-19 18:55:56 +0545

    add buttons and  cases when to show and when not to show

--- sftp_workflow_automation/models/ResConfigSettings.py
+++ sftp_workflow_automation/models/ResConfigSettings.py
@@ -1,6 +1,9 @@
 # my_module/models/res_config_settings.py
 from odoo import models, fields
+from pathlib import Path
 
+
+MODULE_NAME = Path(__file__).parent.parent.name
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
@@ -14,20 +17,20 @@ class ResConfigSettings(models.TransientModel):
     def set_values(self):
         super().set_values()
         params = self.env["ir.config_parameter"].sudo()
-        params.set_param("my_module.sftp_hostname", self.sftp_hostname or "")
-        params.set_param("my_module.sftp_port", self.sftp_port or 22)
-        params.set_param("my_module.sftp_username", self.sftp_username or "")
-        params.set_param("my_module.sftp_password", self.sftp_password or "")
-        params.set_param("my_module.sftp_remote_path", self.sftp_remote_path or "")
+        params.set_param(f"{MODULE_NAME}.sftp_hostname", self.sftp_hostname or "")
+        params.set_param(f"{MODULE_NAME}.sftp_port", self.sftp_port or 22)
+        params.set_param(f"{MODULE_NAME}.sftp_username", self.sftp_username or "")
+        params.set_param(f"{MODULE_NAME}.sftp_password", self.sftp_password or "")
+        params.set_param(f"{MODULE_NAME}.sftp_remote_path", self.sftp_remote_path or "")
 
     def get_values(self):
         res = super().get_values()
         params = self.env["ir.config_parameter"].sudo()
         res.update(
-            sftp_hostname=params.get_param("my_module.sftp_hostname", ""),
-            sftp_port=int(params.get_param("my_module.sftp_port", 22)),
-            sftp_username=params.get_param("my_module.sftp_username", ""),
-            sftp_password=params.get_param("my_module.sftp_password", ""),
-            sftp_remote_path=params.get_param("my_module.sftp_remote_path", ""),
+            sftp_hostname=params.get_param(f"{MODULE_NAME}.sftp_hostname", ""),
+            sftp_port=int(params.get_param(f"{MODULE_NAME}.sftp_port", 22)),
+            sftp_username=params.get_param(f"{MODULE_NAME}.sftp_username", ""),
+            sftp_password=params.get_param(f"{MODULE_NAME}.sftp_password", ""),
+            sftp_remote_path=params.get_param(f"{MODULE_NAME}.sftp_remote_path", ""),
         )
         return res
--- sftp_workflow_automation/models/SaleOrder.py
+++ sftp_workflow_automation/models/SaleOrder.py
@@ -4,5 +4,7 @@ from odoo import models, fields, api
 class InheritSaleOrder(models.Model):
     _inherit = "sale.order"
 
+    send_to_3pl = fields.Boolean("Send to 3PL?")
+
     def generate_csv_to_3pl(self):
         print("Button clicked in Sale Order Line")
